a026efeff86e17f8e1800156fa86cc37bc153c76
Ian Jung <inhwan.jung@gmail.com>
2023-03-14 22:01:46 +0900

    2023_03_14_10_01_PM

--- dags/auto-trader.py
+++ dags/auto-trader.py
@@ -5,7 +5,7 @@ import datetime as dt
 from tasks import file1
 from airflow.models import Variable
 from airflow.operators.bash import BashOperator
-
+from airflow.operators.empty import EmptyOperator
 
 default_args = {
     'owner': 'airflow',
@@ -26,10 +26,44 @@ dag = DAG(
     schedule="30 20 * * *"
 )
 
+start_dag = EmptyOperator(
+    task_id='start_dag',
+    dag=dag
+)
+
+end_dag = EmptyOperator(
+    task_id='end_dag',
+    dag=dag
+)
+
+
 t1 = BashOperator(
-    task_id=f"stock-auto-trader",
+    task_id='print_start_datetime',
+    bash_command='date',
+    dag=dag
+)
+
+t2 = BashOperator(
+    task_id=f"auto-stock-trader",
     bash_command=f"docker run invest-to-stock:0.1",
     dag=dag
 )
 
-t1
+t3 = BashOperator(
+    task_id=f"auto-bitcoin-trader",
+    bash_command=f"docker run invest-to-bitcoin:0.5",
+    dag=dag
+)
+
+t4 = BashOperator(
+    task_id='print_end_datetime',
+    bash_command='date',
+    dag=dag
+)
+
+start_dag >> t1
+
+t1 >> t2 >> t4
+t1 >> t3 >> t4
+
+t4 >> end_dag
