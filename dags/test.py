913514693191529c79156f97d1e77effb4b290aa
Ian Jung <inhwan.jung@gmail.com>
2023-03-21 16:54:42 +0900

    2023_03_21_04_54_오후

---
+++ dags/test.py
@@ -0,0 +1,64 @@
+import pendulum
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+# tz = pendulum.timezone("America/New_York")
+tz = pendulum.timezone("Asia/Seoul")
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['inhwan.jung@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=5),
+
+}
+
+
+dag = DAG(
+    dag_id='test',
+    schedule="50 8 * * *",
+    start_date=dt.datetime(2023, 3, 18, tzinfo=tz),
+    default_args=default_args,
+)
+
+
+start_dag = EmptyOperator(
+    task_id='start_dag',
+    dag=dag
+)
+
+end_dag = EmptyOperator(
+    task_id='end_dag',
+    dag=dag
+)
+
+
+t1 = BashOperator(
+    task_id='print_start_datetime',
+    bash_command='date',
+    dag=dag
+)
+
+t2 = BashOperator(
+    task_id=f"auto-bitcoin-trader",
+    bash_command=f"echo 'bashOperator'; sleep 30",
+    dag=dag
+)
+
+t3 = BashOperator(
+    task_id='print_end_datetime',
+    bash_command='date',
+    dag=dag
+)
+
+start_dag >> t1
+
+t1 >> t2 >> t3
+
+t3 >> end_dag
